098b86fa085b67350f70e7d4f9fd9347baba523d
Tomas Vanek <vanekt@fbl.cz>
2025-11-29 09:37:58 +0100

    gdb_server: preserve gdb monitor command output from Tcl events
    target_call_timer_callbacks_now() in gdb_query_packet() may result
    in Tcl event execution. The event output replaces the desired command
    output.
    
    Call target_call_timer_callbacks_now() as late as Tcl return message
    of monitor command is gathered.
    
    Change-Id: Id1c2304fc9b218ceade02e9ae8eff5bd814baa38
    Signed-off-by: Tomas Vanek <vanekt@fbl.cz>

--- src/server/gdb_server.c
+++ src/server/gdb_server.c
@@ -2837,7 +2837,6 @@ static int gdb_query_packet(struct connection *connection,
 			cmd_ctx->current_target_override = saved_target_override;
 
 			current_gdb_connection = NULL;
-			target_call_timer_callbacks_now();
 			gdb_connection->output_flag = GDB_OUTPUT_NO;
 			free(cmd);
 			if (retval == JIM_RETURN)
@@ -2851,6 +2850,9 @@ static int gdb_query_packet(struct connection *connection,
 			} else {
 				retmsg = strdup(cretmsg);
 			}
+
+			target_call_timer_callbacks_now();
+
 			if (!retmsg)
 				return ERROR_GDB_BUFFER_TOO_SMALL;
 
